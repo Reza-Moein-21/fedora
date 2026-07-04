bfdcd970e8d44a47eba1caaee8a1a21a68177363
reza_moein <rezamoeinpe@gmail.com>
2026-07-04 13:36:29 +0330

    yolo

--- setup/defaults-mime.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Setting default using xdg-mime"
-
-xdg-mime default org.gnome.Nautilus.desktop inode/directory
-
-xdg-mime default org.gnome.Loupe.desktop image/png image/jpeg image/webp image/gif image/bmp image/tiff image/avif image/heic image/svg+xml
-
-echo "Done." 
