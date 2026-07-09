665b326aa2d64695f569d186c7e6da25262d395d
reza_moein <rezamoeinpe@gmail.com>
2026-07-09 15:01:14 +0330

    waybar and stuff

--- dotfiles/sway/.config/sway/scripts/apply-theme.sh
+++ dotfiles/sway/.config/sway/scripts/apply-theme.sh
@@ -5,6 +5,5 @@ gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-olive-dark'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface icon-theme 'Yaru-olive-dark'
 gsettings set org.gnome.desktop.interface cursor-theme 'breeze_cursors'
-gsettings set org.gnome.desktop.interface cursor-size 26 
-gsettings set org.gnome.desktop.interface font-name 'Vazirmatn 11'
+gsettings set org.gnome.desktop.interface cursor-size 26
 
